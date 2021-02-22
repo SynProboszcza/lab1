68e1f762c8d4ac968843eef0bc6400ad1391d5f8
SynProboszcza <SynProboszcza>
2021-02-22 14:30:48 +0100

    Przesyłam zadania w jednym pliku, a nie w oddzielnych

---
+++ Zadania/Zadania.py
@@ -0,0 +1,47 @@
+"""
+#Zad1. Napisz pierwszy skrypt, w którym zadeklarujesz po dwie zmienne każdego typu a następnie wyświetl te zmienne
+
+int1 = 1
+int2 = 2
+string1 = "Candy"
+string2 = "Strawberry"
+float1 = 15.3
+float2 = 15.15
+complex1 = 1+1j
+complex2 = 12+12j
+
+print(int1, int2, string1, string2, float1, float2, complex1, complex2)
+"""
+
+#Zad2. Stwórz skrypt kalkulator, w którym wykorzystać wszystkie podstawowe działania arytmetyczne
+from math import *
+
+a = 5
+b = 9
+Pa = sqrt(a)
+Pb = sqrt(b)
+print("Dla zmiennych %d i %d wyniki są następujące: "%(a, b))
+print("Dodawanie a+b = %d" %(a+b))
+print("Odejmowanie a-b = %d" %(a*b))
+print("Mnożenie a*b = %d" %(a*b))
+print("Dzielenie a/b = %d" %(a/b))
+print("Potęgowanie a^b = %d" %(a**b))
+print("Pierwiastkowanie(stopnia drugiego) sqrt(a) = %d, sqrt(b) = %d" %(Pa, Pb))
+print("Dzielenie całkowite a//b = %d" %(a//b))
+print("Reszta z dzielenia a%%b = %d" %(a%b))
+print("Potęgowanie pow(b,a) = %d" %(pow(b,a)))
+print("a++ = %d" %(a+1))
+print("b++ = %d" %(b+1))
+print("Zaokrąglenie do całości w gół = %d" %())
+print("Zaokrąglenie do całości w górę = %d" %())
+print("sin(a) = %d" %(sin(a)))
+print("sin(b) = %d" %(sin(b)))
+print("cos(a) = %d" %(cos(a)))
+print("cos(b) = %d" %(cos(b)))
+"""
+
+
+
+
+
+
